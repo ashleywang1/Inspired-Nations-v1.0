6b3522cea3f4052173113b40fb9a56f4bb1fb7a1
ashleywang1 <ashwang@mit.edu>
2014-06-09 21:33:04 -0400

    updated Theme

--- Inspired Nations v1.0/src/com/github/InspiredOne/InspiredNations/ToolBox/Theme.java
+++ Inspired Nations v1.0/src/com/github/InspiredOne/InspiredNations/ToolBox/Theme.java
@@ -36,7 +36,6 @@ public class Theme {
 	public void setHEADER(String hEADER) {
 		HEADER = hEADER;
 	}
-<<<<<<< Upstream, based on jeds_version/master
 
 	public String SUBHEADER() {
 		return SUBHEADER;
@@ -157,73 +156,5 @@ public class Theme {
 	public void setENDINSTRUCT(String eNDINSTRUCT) {
 		ENDINSTRUCT = eNDINSTRUCT;
 	}
-=======
-	
-	/**
-	 * Header
-	 */
-	public abstract String HEADER();
-	/**
-	 * Subheader
-	 */
-	public abstract String SUBHEADER();
-	/**
-	 * Used to say what the proceeding value is.
-	 */
-	public abstract String LABEL();
-	/**
-	 * Anything that is calculated information displayed for the viewer 
-	 */
-	public abstract String VALUE();
-	/**
-	 * Additional text describing the value
-	 */
-	public abstract String VALUEDESCRI();
-	/**
-	 * Used for the divider between sections of the HUD
-	 */
-	public abstract String DIVIDER();
-	
-	/**
-	 * The color of the options
-	 */
-	public abstract String OPTION();
-	/**
-	 * The color of the number correlating to the option
-	 */
-	public abstract String OPTIONNUMBER();
-	/**
-	 * Used for text that describes an option
-	 */
-	public abstract String OPTIONDESCRIP();
-	/**
-	 * Color to be used for an unavailable option
-	 */
-	public abstract String UNAVAILABLE();
-	/**
-	 * Color of text next to unavailable option describing why it's unavailable
-	 */
-	public abstract String UNAVAILREASON();
-	/**
-	 * Used to give instructions
-	 */
-	public abstract String INSTRUCTION();
-	/**
-	 * Used for error messages through out the plugin
-	 */
-	public abstract String ERROR();
-	/**
-	 * Used for alert messages through out the plugin
-	 */
-	public abstract String ALERT();
-	/**
-	 * Used for all units mentioned in the HUD
-	 */
-	public abstract String UNIT();
-	/**
-	 * Used for the line of text below the HUD
-	 */
-	public abstract String ENDINSTRU();
 
->>>>>>> eec44e3 Color Options
 }
