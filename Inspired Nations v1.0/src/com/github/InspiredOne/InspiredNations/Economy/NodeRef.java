181ffa3bedd4e2c9bfa4bb36148db1721ec3e026
InspiredOne <inspired@mit.edu>
2014-06-22 22:02:37 -0500

    Fixed all the Itemstacks. No more deprecated code in the economy section.

--- Inspired Nations v1.0/src/com/github/InspiredOne/InspiredNations/Economy/NodeRef.java
+++ Inspired Nations v1.0/src/com/github/InspiredOne/InspiredNations/Economy/NodeRef.java
@@ -104,12 +104,12 @@ public class NodeRef {
 									item.goldIngot
 							})
 						}),
-						new ItemNode( new ItemStack(308, 1, (short) 0), new Node[] { // Iron Leggings
+						new ItemNode( new ItemStack(Material.IRON_LEGGINGS, 1, (short) 0), new Node[] { // Iron Leggings
 							new PerfectCompNode( new double[] {7}, new Node[] {
 									item.ironIngot
 							})
 						}),
-						new ItemNode( new ItemStack(312, 1, (short) 0), new Node[] { // Diamond Leggings
+						new ItemNode( new ItemStack(Material.DIAMOND_LEGGINGS, 1, (short) 0), new Node[] { // Diamond Leggings
 							new PerfectCompNode( new double[] {7}, new Node[] {
 								item.diamond
 							})
@@ -118,27 +118,27 @@ public class NodeRef {
 				// }
 				// { Boots
 				new PerfectSubNode( new double[] {1,3,4,6,10}, new Node[] { // Boots
-						new ItemNode( new ItemStack(301, 1, (short) 0), new Node[] { // Leather Leggings
+						new ItemNode( new ItemStack(Material.LEATHER_BOOTS, 1, (short) 0), new Node[] { // Leather Boots
 							new PerfectCompNode( new double[] {4}, new Node[] {
-								new ItemNode( new ItemStack(334, 1, (short) 0)) // Leather
+								new ItemNode( new ItemStack(Material.LEATHER, 1, (short) 0)) // Leather
 							})
 						}),
-						new ItemNode( new ItemStack(305, 1, (short) 0), new Node[] { // Chain Leggings
+						new ItemNode( new ItemStack(Material.CHAINMAIL_BOOTS, 1, (short) 0), new Node[] { // Chain Boots
 							new PerfectCompNode( new double[] {4}, new Node[] {
-								new ItemNode( new ItemStack(51, 1, (short) 0)) // Fire
+								new ItemNode( new ItemStack(Material.FIRE, 1, (short) 0)) // Fire
 							})
 						}),
-						new ItemNode( new ItemStack(317, 1, (short) 0), new Node[] { // Gold Leggings
+						new ItemNode( new ItemStack(Material.GOLD_BOOTS, 1, (short) 0), new Node[] { // Gold Boots
 							new PerfectCompNode( new double[] {4}, new Node[] {
 									item.goldIngot
 							})
 						}),
-						new ItemNode( new ItemStack(309, 1, (short) 0), new Node[] { // Iron Leggings
+						new ItemNode( new ItemStack(Material.IRON_BOOTS, 1, (short) 0), new Node[] { // Iron Boots
 							new PerfectCompNode( new double[] {4}, new Node[] {
 									item.ironIngot
 							})
 						}),
-						new ItemNode( new ItemStack(313, 1, (short) 0), new Node[] { // Diamond Leggings
+						new ItemNode( new ItemStack(Material.DIAMOND_BOOTS, 1, (short) 0), new Node[] { // Diamond Boots
 							new PerfectCompNode( new double[] {4}, new Node[] {
 								item.diamond
 							})
@@ -151,31 +151,31 @@ public class NodeRef {
 			new CobDugNode( new double[] {5,4,3.5,2,1}, new Node[] { // Tools
 					// { Pickaxe
 					new PerfectSubNode( new double[] {1,2,2.5,5,7}, new Node[] { // Pickaxe
-							new ItemNode( new ItemStack(270, 1, (short) 0), new Node[] { // Wooden Pickaxe
+							new ItemNode( new ItemStack(Material.WOOD_PICKAXE, 1, (short) 0), new Node[] { // Wooden Pickaxe
 								new PerfectCompNode( new double[] {2,3}, new Node[] {
 										item.stick,
 										item.plank
 								})
 							}),
-							new ItemNode( new ItemStack(274, 1, (short) 0), new Node[] { // Stone Pickaxe
+							new ItemNode( new ItemStack(Material.STONE_PICKAXE, 1, (short) 0), new Node[] { // Stone Pickaxe
 								new PerfectCompNode( new double[] {2,3}, new Node[] {
 										item.stick,
 										new ItemNode( new ItemStack(Material.COBBLESTONE, 1, (short) 0)) // Cobblestone
 								})
 							}),
-							new ItemNode( new ItemStack(285, 1, (short) 0), new Node[] { // Gold Pickaxe
+							new ItemNode( new ItemStack(Material.GOLD_PICKAXE, 1, (short) 0), new Node[] { // Gold Pickaxe
 								new PerfectCompNode( new double[] {2,3}, new Node[] {
 										item.stick,
 										item.goldIngot
 								})
 							}),
-							new ItemNode( new ItemStack(257, 1, (short) 0), new Node[] { // Iron Pickaxe
+							new ItemNode( new ItemStack(Material.IRON_PICKAXE, 1, (short) 0), new Node[] { // Iron Pickaxe
 								new PerfectCompNode( new double[] {2,3}, new Node[] {
 										item.stick,
 										item.ironIngot
 								})
 							}),
-							new ItemNode( new ItemStack(278, 1, (short) 0), new Node[] { // Diamond Pickaxe
+							new ItemNode( new ItemStack(Material.DIAMOND_PICKAXE, 1, (short) 0), new Node[] { // Diamond Pickaxe
 								new PerfectCompNode( new double[] {2,3}, new Node[] {
 										item.stick,
 										item.diamond
@@ -185,31 +185,31 @@ public class NodeRef {
 					// }
 					// { Shovel
 					new PerfectSubNode( new double[] {1,2,4,5,7}, new Node[] { // Shovel
-							new ItemNode( new ItemStack(269, 1, (short) 0), new Node[] { // Wooden Shovel
+							new ItemNode( new ItemStack(Material.WOOD_SPADE, 1, (short) 0), new Node[] { // Wooden Shovel
 								new PerfectCompNode( new double[] {2,1}, new Node[] {
 										item.stick,
 										item.plank
 								})
 							}),
-							new ItemNode( new ItemStack(273, 1, (short) 0), new Node[] { // Stone Shovel
+							new ItemNode( new ItemStack(Material.STONE_SPADE, 1, (short) 0), new Node[] { // Stone Shovel
 								new PerfectCompNode( new double[] {2,1}, new Node[] {
 										item.stick,
-										new ItemNode( new ItemStack(4, 1, (short) 0)) // Cobblestone
+										new ItemNode( new ItemStack(Material.COBBLESTONE, 1, (short) 0)) // Cobblestone
 								})
 							}),
-							new ItemNode( new ItemStack(284, 1, (short) 0), new Node[] { // Gold Shovel
+							new ItemNode( new ItemStack(Material.GOLD_SPADE, 1, (short) 0), new Node[] { // Gold Shovel
 								new PerfectCompNode( new double[] {2,1}, new Node[] {
 										item.stick,
 										item.goldIngot
 								})
 							}),
-							new ItemNode( new ItemStack(256, 1, (short) 0), new Node[] { // Iron Shovel
+							new ItemNode( new ItemStack(Material.IRON_SPADE, 1, (short) 0), new Node[] { // Iron Shovel
 								new PerfectCompNode( new double[] {2,1}, new Node[] {
 										item.stick,
 										item.ironIngot
 								})
 							}),
-							new ItemNode( new ItemStack(277, 1, (short) 0), new Node[] { // Diamond Shovel
+							new ItemNode( new ItemStack(Material.DIAMOND_SPADE, 1, (short) 0), new Node[] { // Diamond Shovel
 								new PerfectCompNode( new double[] {2,1}, new Node[] {
 										item.stick,
 										item.diamond
@@ -219,31 +219,31 @@ public class NodeRef {
 					// }
 					// { Axe
 					new PerfectSubNode( new double[] {1,2,4,5,7}, new Node[] { // Axe
-							new ItemNode( new ItemStack(271, 1, (short) 0), new Node[] { // Wooden Axe
+							new ItemNode( new ItemStack(Material.WOOD_AXE, 1, (short) 0), new Node[] { // Wooden Axe
 								new PerfectCompNode( new double[] {2,3}, new Node[] {
 										item.stick,
 										item.plank
 								})
 							}),
-							new ItemNode( new ItemStack(275, 1, (short) 0), new Node[] { // Stone Axe
+							new ItemNode( new ItemStack(Material.STONE_AXE, 1, (short) 0), new Node[] { // Stone Axe
 								new PerfectCompNode( new double[] {2,3}, new Node[] {
 										item.stick,
-										new ItemNode( new ItemStack(4, 1, (short) 0)) // Cobblestone
+										new ItemNode( new ItemStack(Material.COBBLESTONE, 1, (short) 0)) // Cobblestone
 								})
 							}),
-							new ItemNode( new ItemStack(286, 1, (short) 0), new Node[] { // Gold Axe
+							new ItemNode( new ItemStack(Material.GOLD_AXE, 1, (short) 0), new Node[] { // Gold Axe
 								new PerfectCompNode( new double[] {2,3}, new Node[] {
 										item.stick,
 										item.goldIngot
 								})
 							}),
-							new ItemNode( new ItemStack(258, 1, (short) 0), new Node[] { // Iron Axe
+							new ItemNode( new ItemStack(Material.IRON_AXE, 1, (short) 0), new Node[] { // Iron Axe
 								new PerfectCompNode( new double[] {2,3}, new Node[] {
 										item.stick,
 										item.ironIngot
 								})
 							}),
-							new ItemNode( new ItemStack(279, 1, (short) 0), new Node[] { // Diamond Axe
+							new ItemNode( new ItemStack(Material.DIAMOND_AXE, 1, (short) 0), new Node[] { // Diamond Axe
 								new PerfectCompNode( new double[] {2,3}, new Node[] {
 										item.stick,
 										item.diamond
@@ -252,7 +252,7 @@ public class NodeRef {
 					}),
 					// }
 					// { Shears
-					new ItemNode( new ItemStack(359, 1, (short) 0), new Node[] { // Shears
+					new ItemNode( new ItemStack(Material.SHEARS, 1, (short) 0), new Node[] { // Shears
 						new PerfectCompNode( new double[] {2}, new Node[] {
 								item.ironIngot
 						})
@@ -260,31 +260,31 @@ public class NodeRef {
 					// }
 					// { Hoe
 					new PerfectSubNode( new double[] {1,2,2.3,2.3,2.4}, new Node[] { // Hoe
-							new ItemNode( new ItemStack(290, 1, (short) 0), new Node[] { // Wooden Hoe
+							new ItemNode( new ItemStack(Material.WOOD_HOE, 1, (short) 0), new Node[] { // Wooden Hoe
 								new PerfectCompNode( new double[] {2,2}, new Node[] {
 										item.stick,
 										item.plank
 								})
 							}),
-							new ItemNode( new ItemStack(291, 1, (short) 0), new Node[] { // Stone Hoe
+							new ItemNode( new ItemStack(Material.STONE_HOE, 1, (short) 0), new Node[] { // Stone Hoe
 								new PerfectCompNode( new double[] {2,2}, new Node[] {
 										item.stick,
-										new ItemNode( new ItemStack(4, 1, (short) 0)) // Cobblestone
+										new ItemNode( new ItemStack(Material.COBBLESTONE, 1, (short) 0)) // Cobblestone
 								})
 							}),
-							new ItemNode( new ItemStack(294, 1, (short) 0), new Node[] { // Gold Hoe
+							new ItemNode( new ItemStack(Material.GOLD_HOE, 1, (short) 0), new Node[] { // Gold Hoe
 								new PerfectCompNode( new double[] {2,2}, new Node[] {
 										item.stick,
 										item.goldIngot
 								})
 							}),
-							new ItemNode( new ItemStack(292, 1, (short) 0), new Node[] { // Iron Hoe
+							new ItemNode( new ItemStack(Material.IRON_HOE, 1, (short) 0), new Node[] { // Iron Hoe
 								new PerfectCompNode( new double[] {2,2}, new Node[] {
 										item.stick,
 										item.ironIngot
 								})
 							}),
-							new ItemNode( new ItemStack(293, 1, (short) 0), new Node[] { // Diamond Hoe
+							new ItemNode( new ItemStack(Material.DIAMOND_HOE, 1, (short) 0), new Node[] { // Diamond Hoe
 								new PerfectCompNode( new double[] {2,2}, new Node[] {
 										item.stick,
 										item.diamond
