04a31e6c5d087be3157a99f7e3aaf9152eb61ef6
InspiredOne <inspired@mit.edu>
2014-06-25 23:12:43 -0500

    Actually added the Accept request and Deny Request options for Owner

--- Inspired Nations v1.0/src/com/github/InspiredOne/InspiredNations/Hud/Implem/ManageGov/ResidentControl/RequestsForOwner.java
+++ Inspired Nations v1.0/src/com/github/InspiredOne/InspiredNations/Hud/Implem/ManageGov/ResidentControl/RequestsForOwner.java
@@ -5,6 +5,9 @@ import com.github.InspiredOne.InspiredNations.Governments.OwnerGov;
 import com.github.InspiredOne.InspiredNations.Hud.Menu;
 import com.github.InspiredOne.InspiredNations.Hud.PromptOption;
 import com.github.InspiredOne.InspiredNations.Hud.TabSelectOptionMenu;
+import com.github.InspiredOne.InspiredNations.Hud.Implem.IgnoreOfferOption;
+import com.github.InspiredOne.InspiredNations.Hud.Implem.JoinOwnerGovOption;
+import com.github.InspiredOne.InspiredNations.Hud.Implem.JoinSubjectGovOption;
 import com.github.InspiredOne.InspiredNations.Hud.Implem.Player.PlayerID;
 
 public class RequestsForOwner extends TabSelectOptionMenu<PlayerID> {
@@ -42,6 +45,10 @@ public class RequestsForOwner extends TabSelectOptionMenu<PlayerID> {
 
 	@Override
 	public void addOptions() {
+		if(taboptions.size() != 0) {
+			this.options.add(new IgnoreOfferOption(this, "Ignore Request", this.getData(), gov.getOwnerRequests()));
+			this.options.add(new JoinOwnerGovOption(this, "Accept Request", this.gov, this.getData()));
+		}
 		this.options.add(new PromptOption(new RequestsForOwner(PDI, previous, gov), gov.getOwnerPositionName() + " Offers",
 				new OffersForOwner(PDI, previous, gov)));
 		
@@ -49,8 +56,7 @@ public class RequestsForOwner extends TabSelectOptionMenu<PlayerID> {
 
 	@Override
 	public void addActionManagers() {
-		// TODO Auto-generated method stub
-		
+
 	}
 
 }
